02949b06e13115dbfb2637611bbdac836f303f95
Kevin Zong <7kevinzong@gmail.com>
2025-02-25 21:12:24 -0500

    implement train_and_evaluate

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 from ml_code.data_load import load_data
 from ml_code.pre_processing import preprocess_data
 from ml_code.models import ModelFactory
-# from ml_code.train import train_and_evaluate
+from ml_code.train import train_and_evaluate
 # from ml_code.metrics import print_metrics
 # from test.unit_test import TestDataLoader
 
@@ -22,7 +22,7 @@ X_train, X_test, y_train, y_test = preprocess_data(data)
 model = ModelFactory.get_model(config["model_type"])
 
 # # Step 4: Train and evaluate the model
-# accuracy, cm, y_test, y_prob = train_and_evaluate(model, X_train, X_test, y_train, y_test)
+accuracy, cm, y_test, y_prob = train_and_evaluate(model, X_train, X_test, y_train, y_test)
 
 # # Step 5: Print metrics
 # print_metrics(accuracy, cm, y_test, y_prob)
