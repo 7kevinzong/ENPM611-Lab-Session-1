ff64755b5c23552ff129cc59029e8c66539ebe77
Kevin Zong <7kevinzong@gmail.com>
2025-02-25 20:49:15 -0500

    implement ModelFactory

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import json
 from ml_code.data_load import load_data
 from ml_code.pre_processing import preprocess_data
-# from ml_code.models import ModelFactory
+from ml_code.models import ModelFactory
 # from ml_code.train import train_and_evaluate
 # from ml_code.metrics import print_metrics
 # from test.unit_test import TestDataLoader
@@ -19,7 +19,7 @@ print(data.head())
 X_train, X_test, y_train, y_test = preprocess_data(data)
 
 # # Step 3: Get the model from ModelFactory
-# model = ModelFactory.get_model(config("model_type"))
+model = ModelFactory.get_model(config["model_type"])
 
 # # Step 4: Train and evaluate the model
 # accuracy, cm, y_test, y_prob = train_and_evaluate(model, X_train, X_test, y_train, y_test)
